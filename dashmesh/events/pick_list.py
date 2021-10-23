565f48db870a1d43775671bb0458f1790b2a19d1
RoshnaVijayan <RoshnaVijayan>
2021-10-23 12:36:45 +0400

    pick list

--- dashmesh/events/pick_list.py
+++ dashmesh/events/pick_list.py
@@ -4,7 +4,37 @@ from frappe import _
 import frappe.defaults
 
 def set_warehouses(doc, handler =None):
+	loc_list = []
+	item_dict = {}
+	for item in doc.locations:
+		if item.sales_order:
+			if item.item_code not in item_dict.keys():
+				item_dict.update({item.item_code : [item.sales_order_item]})
+				loc_list.append(item)
+			else:
+				if item.sales_order_item not in item_dict[item.item_code]:
+					item_dict[item.item_code].append(item.sales_order_item)
+					loc_list.append(item)
+	doc.locations = loc_list
+
 	for item in doc.locations:
 		if item.sales_order_item:
 			so_item = frappe.get_doc("Sales Order Item",item.sales_order_item)
-			item.warehouse = so_item.warehouse
+			db_exist = frappe.db.exists({
+						"doctype": "Bin",
+						"item_code": item.item_code,
+						"warehouse": so_item.warehouse,
+			})
+			if db_exist:
+				item_bin = frappe.get_doc("Bin",db_exist[0][0])
+				actual_qty = item_bin.actual_qty
+				item.warehouse = so_item.warehouse
+				if actual_qty <= so_item.qty:
+					item.qty = actual_qty
+					item.stock_qty = actual_qty
+					item.picked_qty = actual_qty
+				else:
+					item.qty = so_item.qty
+					item.stock_qty = so_item.qty
+					item.picked_qty = so_item.qty
+					
