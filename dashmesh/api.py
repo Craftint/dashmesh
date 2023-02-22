a9fef59ff6a66139d137113587a4213addfb6634
riane <riane@craftinteractive.ae>
2023-02-22 10:30:52 +0400

    ignore permission

--- dashmesh/api.py
+++ dashmesh/api.py
@@ -11,7 +11,7 @@ def count(name):
     for getting count of Goods In Transit Note for a specific Purchase Order
     """
     unique={}
-    parent_list = frappe.db.get_list("Goods In Transit Item", {"purchase_order": name }, ['parent'])
+    parent_list = frappe.db.get_all("Goods In Transit Item", {"purchase_order": name }, ['parent'])
     if len(parent_list) >0:
         for row in parent_list:
             if row.parent not in unique:
